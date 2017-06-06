f59141d4e2341123c26ee67d307ea322a409348e
cezs <diagram.chasing@gmail.com>
2017-06-06 10:08:15 +0100

    Add template local variables

---
+++ elisp/cs-local.el
@@ -0,0 +1,40 @@
+;;; cs-local.el --- Local data and information
+
+;; Copyright (C) 2016-2017 Cezary Stankiewicz
+
+;; Author: Cezary Stankiewicz (concat "c.stankiewicz" "@" "wlv.ac.uk")
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+
+;;; Commentary:
+
+;; Local data and information.
+
+;;; Code:
+
+(defvar cs-base-directory "~/_local/edu/6/6cs007/work/docs/homepage/blog/posts")
+(defvar cs-publishing-directory "~/_local/edu/6/6cs007/work/docs/homepage/blog/publish")
+(defvar cs-html-link-home "http://mi-linux.wlv.ac.uk/~1427790/final-project/docs/homepage")
+(defvar cs-title "Final Year Project Blog")
+(defvar cs-blog-disqus-shortname "cezs")
+
+(provide 'cs-local)
+
+;;; cs-local.el ends here
